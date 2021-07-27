e23e5782eaa4096dfaeeef6e0b292fff664f74c0
Vinamra Khoria <Vinamrakhoria@gmail.com>
2021-07-27 13:51:48 +0530

    requirements added and readme updated

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # WhatsApp-Automation
 Automates the process of getting the number of participants in a WhatsApp group
 
-To run - execute the main.py script
+To run - <br/>
+* run "pip install -r requirements.txt"
+* download webdriver from here - <https://chromedriver.chromium.org/downloads>
+* replace the <username> in config.py with your username on system
+* add group names in the list in main.py
+* run main.py script
+
--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\vinam\\AppData\\Local\\Google\\Chrome\\User Data\\Wtsp"
+CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\<username>\\AppData\\Local\\Google\\Chrome\\User Data\\Wtsp"
+
+# Windows 7, 8.1, and 10: C:\Users\<username>\AppData\Local\Google\Chrome\User Data\Wtsp
+# Mac OS X El Capitan: Users/<username>/Library/Application Support/Google/Chrome/Wtsp
+# Linux: /home/<username>/.config/google-chrome/Wtsp
