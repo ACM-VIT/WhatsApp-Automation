ab2ab222cc7625b397a63c25b6e19badf47da7b8
aakaash <ssaakaash@icloud.com>
2023-09-23 00:13:23 +0530

    fixes #3

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from tabulate import tabulate
 from auto import numberOfMembers
 
-groups = ["PRP107", "VVIP LOUNGE"]
 #Insert your group names here
+groups = ["PRP107", "VVIP LOUNGE"]
 
-print(numberOfMembers(groups))
+head = ["Group Name", "Number of Participants"]
+members = numberOfMembers(groups)
 
+data = []
+for i in range(len(groups)):
+    data.append([groups[i], members[i]])
+
+print(tabulate(data, headers=head, tablefmt="github"))
