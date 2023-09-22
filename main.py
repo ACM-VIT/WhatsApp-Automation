a606b8958ac7680a6f132435beef8031de028a30
gautam-4 <gautamalt468@gmail.com>
2023-09-22 21:15:08 +0530

    issue#5

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import subprocess
+
+# Run the requirements.py script to install required packages
+subprocess.check_call(["python", "requirements.py"])
+
 from auto import numberOfMembers
 
 groups = ["ACM Research Formal Group","ACM Board Technical","ACM-VIT | INFORMAL"]
