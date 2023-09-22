563c2bb6b6e26f10102fedd7b3f4ace083eac72f
sagnikMajumder89 <sagnikm183@gmail.com>
2023-09-22 22:44:42 +0530

    Fixed issue 10

--- auto.py
+++ auto.py
@@ -18,18 +18,19 @@ def numberOfMembers(groups):
 
     browser.get('https://web.whatsapp.com/')
 
-    time.sleep(10)
-
     xpath = '//div[@contenteditable="true"][@data-tab="3"]'
 
     search_box = WebDriverWait(browser, 500).until(EC.presence_of_element_located((By.XPATH, xpath)))
     
     res = []
+    prev = 0
     for group_name in groups:
         search_box.click()  # Click the div to ensure it has focus
         
         # Issue #10
         # the previous group name must be removed before pasting in new group name
+        search_box.send_keys(Keys.BACK_SPACE*prev)
+        prev = len(group_name)
         
         pyperclip.copy(group_name)
 
@@ -38,13 +39,14 @@ def numberOfMembers(groups):
         search_box.send_keys(Keys.ENTER)
 
         top_xpath = '//*[@id="main"]/header/div[2]/div[1]/div/span'
+        
 
         top_click = WebDriverWait(browser, 1000).until(EC.presence_of_element_located((By.XPATH, top_xpath)))
         top_click.click()
         time.sleep(3)
 
         # Issue #6
-        number_xpath = "" # complete Xpath of the element that contains number of participants.
+        number_xpath = '//*[@id="app"]/div/div/div[6]/span/div/span/div/div/div/section/div[1]/div/div[3]/span/span/button' # complete Xpath of the element that contains number of participants.
         number_of_part = WebDriverWait(browser, 1000).until(EC.presence_of_element_located((By.XPATH, number_xpath)))
 
         num = number_of_part.get_attribute('innerHTML')
