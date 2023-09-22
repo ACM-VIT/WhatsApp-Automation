9af33026f821eb31ea33fa88ba6e2aaf69e0a986
aakaash <ssaakaash@icloud.com>
2023-09-22 23:44:06 +0530

    fixes #7

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-CHROME_DRIVER_PATH = "/Users/aakaash/Downloads/chromedriver-mac-arm64/chromedriver"
+from install_driver import get_platform
+
+CHROME_DRIVER_PATH = f"./chromedriver-{get_platform()}/chromedriver"
 CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 # Download Chrome Driver from https://chromedriver.chromium.org/downloads and paste the link here.
--- requirements.py
+++ requirements.py
@@ -1,4 +1,5 @@
 import subprocess
+from install_driver import install_driver
 
 # Define required packages and their versions
 with open("requirements.txt", "r") as required_packages:
@@ -6,3 +7,5 @@ with open("requirements.txt", "r") as required_packages:
     # Install required packages
     for package in required_packages:
         subprocess.check_call(["pip", "install", package])
+
+install_driver()
