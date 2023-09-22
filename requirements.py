a4c46c21b5e775b5fae5f428f0388d73995c95f3
aakaash <ssaakaash@icloud.com>
2023-09-22 22:53:03 +0530

    fixes #5

---
+++ requirements.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# Define required packages and their versions
+required_packages = [
+    "pyperclip==1.8.2",
+    "selenium==3.141.0",
+    "urllib3==1.26.6",
+]
+
+# Install required packages
+for package in required_packages:
+    subprocess.check_call(["pip", "install", package])
