1ad3abf2b27b7a0e6eba02995ae36c0293a07f93
aakaash <ssaakaash@icloud.com>
2023-09-22 23:01:53 +0530

    fixes #5: Re-added requirements.txt

--- requirements.py
+++ requirements.py
@@ -1,12 +1,8 @@
 import subprocess
 
 # Define required packages and their versions
-required_packages = [
-    "pyperclip==1.8.2",
-    "selenium==3.141.0",
-    "urllib3==1.26.6",
-]
+with open("requirements.txt", "r") as required_packages:
 
-# Install required packages
-for package in required_packages:
-    subprocess.check_call(["pip", "install", package])
+    # Install required packages
+    for package in required_packages:
+        subprocess.check_call(["pip", "install", package])
