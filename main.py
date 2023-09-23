7b502ccaf6f3bb118ec897dd8b709c773e7fa695
Vaibhav Sijaria <139199971+vaibhavsijaria@users.noreply.github.com>
2023-09-23 08:51:32 +0530

    feat: Automatic dependencies install
    Used subprocess library to automaticly install dependencies from requirements.txt file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from auto import numberOfMembers
+import subprocess
+
+subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
 
 groups = ["ACM Research Formal Group","ACM Board Technical","ACM-VIT | INFORMAL"]
 #Insert your group names here
